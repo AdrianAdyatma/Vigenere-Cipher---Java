5ca9a26e0d43d0356fcba20f9258cc8334ef77d5
Adrian-Ryzen <adrianadyatma21@gmail.com>
2018-10-06 21:16:55 +0700

    Decryption finished
    All fixed, manual input for plain and cipher text

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -2,34 +2,28 @@ import java.util.Scanner;
 
 public class VigenereCipher {
     public static void main(String[] args) {
-        System.out.println("\nVIGENERE CIPHER\n\n1. Encryption\n2. Decryption");
-        System.out.print("\nEnter your option number : ");
-        Scanner reader = new Scanner(System.in);
-        char c = reader.next().charAt(0);
 
-        switch (c) {
-            case 1:
-                encrypt();
-                break;
-            case 2:
-                decrypt();
-                break;
-            default:
-                System.out.println("Invalid option");
-                break;
-        }
+        encrypt();
+
+        decrypt();
+
     }
 
     private static void encrypt() {
 
-        String plainText = "THECRAZYPROGRAMMER222";
+        // KALIMAT YANG INGIN DIENKRIPSI
+        String plainText = "THECRAZYPROGRAMMER";
         plainText = formatText(plainText);
+        // KEY YANG INGIN DIGUNAKAN
         String key = "HELLO";
         key = formatText(key);
 
-        System.out.println(plainText);
-        System.out.println(plainText.length());
+        System.out.println("\nKalimat = "+plainText);
+        System.out.println("Panjang kalimat = "+plainText.length());
+        System.out.println("Key = "+key);
+        System.out.println("Panjang key = "+key.length());
 
+        // MEMBUAT KEY BARU BERDASARKAN PANJANG PLAINTEXT
         StringBuilder keyBuilder = new StringBuilder();
         for (int i = 0, j = 0; i < plainText.length(); i++, j++) {
             if (j == key.length()) {
@@ -39,8 +33,10 @@ public class VigenereCipher {
         }
         String newKey = keyBuilder.toString();
 
-        System.out.println(newKey);
+        System.out.println("\nKey baru = "+newKey);
+        System.out.println("Panjang key baru = "+newKey.length());
 
+        // MEMBUAT CIPHERTEXT
         StringBuilder cipherBuilder = new StringBuilder();
         for (int i = 0; i < plainText.length(); i++) {
             char c = plainText.charAt(i);
@@ -55,12 +51,56 @@ public class VigenereCipher {
         }
         String cipherText = cipherBuilder.toString();
 
-        System.out.println(cipherText);
-        System.out.println(cipherText.length());
+        System.out.println("\nCiphertext = "+cipherText);
+        System.out.println("Panjang ciphertext = "+cipherText.length());
 
+        System.out.println("--------------------------------------------");
     }
 
     private static void decrypt() {
+
+        // KALIMAT YANG INGIN DIENKRIPSI
+        String cipherText = "ALPNFHDJAFVKCLATIC";
+        cipherText = formatText(cipherText);
+        // KEY YANG DIGUNAKAN
+        String key = "HELLO";
+        key = formatText(key);
+
+        System.out.println("\nCiphertext = "+cipherText);
+        System.out.println("Panjang ciphertext = "+cipherText.length());
+        System.out.println("Key = "+key);
+        System.out.println("Panjang key = "+key.length());
+
+        // MEMBUAT KEY BARU BERDASARKAN PANJANG CIPHERTEXT
+        StringBuilder keyBuilder = new StringBuilder();
+        for (int i = 0, j = 0; i < cipherText.length(); i++, j++) {
+            if (j == key.length()) {
+                j = 0;
+            }
+            keyBuilder.append(key.charAt(j));
+        }
+        String newKey = keyBuilder.toString();
+
+        System.out.println("\nKey baru = "+newKey);
+        System.out.println("Panjang key baru = "+newKey.length());
+
+        // MEMBUAT PLAINTEXT
+        StringBuilder textBuilder = new StringBuilder();
+        for (int i = 0; i < cipherText.length(); i++) {
+            char c = cipherText.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+//            if (c >= '!' && c <= '`'){
+//                c = (char) (((cipherText.charAt(i) + newKey.charAt(i) + 94) % 94)+'!');
+                c = (char) (((cipherText.charAt(i) - newKey.charAt(i) + 26) % 26) + 'A');
+                textBuilder.append(c);
+            } else {
+                textBuilder.append(c);
+            }
+        }
+        String plainText = textBuilder.toString();
+
+        System.out.println("\nPlaintext = "+plainText);
+        System.out.println("Panjang plaintext = "+plainText.length());
 
     }
 
